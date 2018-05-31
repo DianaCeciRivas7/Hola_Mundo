600904f48ac62a3d10007970e9a92e10d4e3f7e4
user <user@com2pc10>
2018-05-31 04:56:24 -0600

    Committttt

--- src/hola/Hola.java
+++ src/hola/Hola.java
@@ -18,7 +18,7 @@ public class Hola {
      */
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            JOptionPane.showMessageDialog(null, "Presione aceptar para llegar a la opcion "+i+".");
+            JOptionPane.showMessageDialog(null, "Presione aceptar para llegar a la opcion "+(i+1)+".");
             
         }
     }
